af02aec10364cf8aab83923d3e0603bd2f98e2b6
HomeChaos <ibi692009@yandex.ru>
2024-11-05 20:04:53 +0300

    new traps

---
+++ Assets/Scripts/Traps/Eagle.cs
@@ -0,0 +1,68 @@
+﻿using DG.Tweening;
+using LevelSystem;
+using UnityEngine;
+
+namespace Traps
+{
+    public class Eagle : Resettable
+    {
+        [SerializeField] private Transform _left;
+        [SerializeField] private Transform _right;
+        [SerializeField] private GameObject _target;
+        [SerializeField] private float _speed = 1f;
+        [SerializeField] private float _scale = 1;
+        [SerializeField] private LoopType _loopType;
+
+        private Sequence _sequence;
+
+        private void Start()
+        {
+            ApplyReset();
+        }
+        
+        public override void ApplyReset()
+        {
+            if (_sequence != null)
+            {
+                _sequence.Kill();
+            }
+
+            ResetPosition();
+            DoPatrol();
+        }
+
+        private void ResetPosition()
+        {
+            UpdateLookForward(true);
+            _target.transform.DOMove(_right.position, 0);
+        }
+
+        private void DoPatrol()
+        {
+            _sequence = DOTween.Sequence();
+
+            _sequence.Append(_target.transform
+                .DOMove(_left.position, _speed)
+                .OnComplete(() => UpdateLookForward(false))
+                .SetEase(Ease.Linear));
+            _sequence.Append(_target.transform
+                .DOMove(_right.position, _speed)
+                .OnComplete(() => UpdateLookForward(true))
+                .SetEase(Ease.Linear));
+
+            _sequence.SetLoops(-1, _loopType).SetEase(Ease.Linear);
+        }
+
+        private void UpdateLookForward(bool isLookLeft)
+        {
+            if (isLookLeft)
+            {
+                _target.transform.localScale = new Vector3(_scale, _scale, _scale);
+            }
+            else
+            {
+                _target.transform.localScale = new Vector3(-_scale, _scale, _scale);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/FallingFloor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using LevelSystem;
+using UnityEngine;
+
+namespace Traps
+{
+    public class FallingFloor : Resettable
+    {
+        [SerializeField] private Collider2D _collider;
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float _delay;
+        [SerializeField] private float _timeToFly;
+
+        private Coroutine _animationCoroutine;
+        private Vector3 _startPosition;
+
+        private void Start()
+        {
+            _startPosition = _rigidbody2D.gameObject.transform.position;
+        }
+
+        public override void ApplyReset()
+        {
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+                _animationCoroutine = null;
+            }
+
+            _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+            _rigidbody2D.velocity = Vector2.zero;
+            _collider.isTrigger = false;
+            _rigidbody2D.gameObject.transform.position = _startPosition;
+        }
+
+        public void Activate()
+        {
+            if (_animationCoroutine != null)
+                return;
+            
+            _animationCoroutine = StartCoroutine(Apply());
+        }
+
+        private IEnumerator Apply()
+        {
+            yield return new WaitForSeconds(_delay);
+
+            _collider.isTrigger = true;
+            _rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+
+            yield return new WaitForSeconds(_timeToFly);
+            _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+            _rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
